91ae8ae6cf3ee2df3dba832a357f3669b9c4af57
mkd92 <manikandan.m1116@gmail.com>
2022-10-23 09:29:43 +0530

    header ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from "./Header";
 
+import Sidebar from "./Sidebar";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline.js";
+import "./Header.css";
 
 function Header() {
   return (
@@ -9,8 +12,13 @@ function Header() {
         <Avatar className="header__avatar" alt="mkd92" src="" />
         <AccessTimeIcon />
       </div>
-      <div className="header__search"></div>
-      <div className="header__right"></div>
+      <div className="header__search">
+        <SearchIcon />
+        <input type="text" placeholder="Search Clever Programmer" />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
     </div>
   );
 }
